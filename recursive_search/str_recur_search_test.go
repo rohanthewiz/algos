f592d683482e9948c483b7443f23a7ae9ce44c1f
Rohan Allison <rohan.allison@cbre.com>
2020-01-14 21:15:36 -0600

    Add string recursive search
    - This proves that unless the dataset is extremely large, linear search is the way to go!

---
+++ recursive_search/str_recur_search_test.go
@@ -0,0 +1,52 @@
+package recursive_search
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+)
+
+const needle = "god"
+
+var haystack []string
+
+func TestMain(m *testing.M) {
+	for i := 0; i < 2000; i++ {
+		haystack = append(haystack, []string{"tac", "esuom", "horse", "mouse", "cat", "dog"}...)
+	}
+	haystack = append(haystack, []string{"frog", "god", "esroh", "elephant", "tiger"}...)
+	haystack = append(haystack, []string{"frog", "ged", "esroh", "elephant", "tiger"}...)
+
+	log.SetOutput(ioutil.Discard)
+	os.Exit(m.Run())
+}
+
+func TestStringRecursiveSearch(t *testing.T) {
+	result := StartStrRecursiveSearch(haystack, needle)
+	if result == -1 {
+		t.Errorf("needle '%s' not found in haystack: %q\n", needle, haystack)
+	} else {
+		t.Logf("Target string: '%s' found at index %d\n", needle, result)
+	}
+}
+
+func BenchmarkStartStrRecursiveSearch(b *testing.B) {
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		StartStrRecursiveSearch(haystack, needle)
+	}
+}
+
+func BenchmarkLinearSearch(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		for i := 0; i < len(haystack); i++ {
+			if haystack[i] == needle {
+				//log.Println("found")
+				break
+			}
+		}
+	}
+}
---
+++ recursive_search/str_recurive_search.go
@@ -0,0 +1,53 @@
+package recursive_search
+
+// Returns index of found string else -1
+func StartStrRecursiveSearch(arr []string, target string) (result int) {
+	return strRecurSearch(arr, target, 0, len(arr))
+}
+
+// Recursive concurrent search
+// arr - array of string to search, lIdx - lower index, uIdx - upper index,
+// result - index of found target else -1
+func strRecurSearch(arr []string, target string, lIdx, uIdx int) (result int) {
+	const batchSize = 512
+
+	//log.Println(lIdx, ",", uIdx)
+
+	// Safeties
+	if lIdx < 0 || lIdx >= uIdx {
+		//log.Println("Invalid indices", lIdx, uIdx)
+		return -1
+	}
+
+	// If at or less than the batchSize then let the machine scan
+	if uIdx-lIdx <= batchSize {
+		for i := lIdx; i < uIdx; i++ {
+			if arr[i] == target {
+				//log.Println("Found in this batch (index):", lIdx, ",", uIdx)
+				return i
+			}
+		}
+		return -1
+	}
+
+	resultChan := make(chan int, 2)
+	mid := lIdx + (uIdx-lIdx)/2
+
+	go func(l, u int) {
+		resultChan <- strRecurSearch(arr, target, l, u)
+	}(lIdx, mid)
+
+	go func(l, u int) {
+		resultChan <- strRecurSearch(arr, target, l, u)
+	}(mid, uIdx)
+
+	for i := 0; i < 2; i++ {
+		select {
+		case r := <-resultChan:
+			if r != -1 { // found!
+				return r
+			}
+		}
+	}
+	return -1
+}
