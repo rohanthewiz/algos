0142703a05000062d708837d3f151bd279f2b919
rohanthewiz <rohanthewiz@gmail.com>
2020-05-31 02:07:59 -0500

    Add Memoized Iterative Fibonacci function for 3 step staircase

--- fib/fib_memo.go
+++ fib/fib_memo.go
@@ -1,7 +1,10 @@
 package fib
 
+// In it's purest form there is no special treatment of 2
+// The pre-established values are 0 -> 0, 1 -> 1
+// Two is really fib(n-1) + fib(n-2) --> fib(1) + fib(0) => 1
 func FibMemo(n int) int {
-	if n == 0 {
+	if n < 1 {
 		return 0
 	}
 	if n <= 2 {
@@ -14,6 +17,31 @@ func FibMemo(n int) int {
 
 	for i := 3; i <= n; i++ {
 		memo[i] = memo[i-1] + memo[i-2]
+	}
+
+	return memo[n]
+}
+
+// Solution for staircase example with up to 3 steps at a time
+// TODO - Medium article
+// In it's purest form there is no special treatment of 2
+// The pre-established values are 0 -> 0, 1 -> 1
+// Two is really fib(n-1) + fib(n-2) --> fib(1) + fib(0) => 1
+func Fib3Memo(n int) int {
+	if n < 1 {
+		return 0
+	}
+	if n <= 2 {
+		return 1
+	}
+
+	memo := make(map[int]int, n) // preallocate the map's capacity
+	memo[1] = 1
+	memo[2] = 1
+	memo[3] = 2
+
+	for i := 4; i <= n; i++ {
+		memo[i] = memo[i-1] + memo[i-2] + memo[i-3]
 	}
 
 	return memo[n]
--- fib/fib_test.go
+++ fib/fib_test.go
@@ -9,6 +9,7 @@ import (
 
 // fibs holds the fib for each index
 var fibs = []int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34}
+var fib3s = []int{0, 1, 1, 2, 4, 7, 13, 24, 44, 81}
 
 func TestFib(t *testing.T) {
 	wg := sync.WaitGroup{}
@@ -50,6 +51,16 @@ func TestFib(t *testing.T) {
 			fmt.Printf("i: %d, FibMemo elasped %s\n", i, time.Now().Sub(t0))
 			if f != fibs[i] {
 				t.Errorf("FibMemo(%d): expected %d, got %d\n", i, fibs[i], f)
+			}
+			wg.Done()
+		}(i, time.Now())
+
+		wg.Add(1)
+		go func(i int, t0 time.Time) {
+			f := Fib3Memo(i)
+			fmt.Printf("i: %d, Fib3Memo elasped %s\n", i, time.Now().Sub(t0))
+			if f != fib3s[i] {
+				t.Errorf("FibM3emo(%d): expected %d, got %d\n", i, fib3s[i], f)
 			}
 			wg.Done()
 		}(i, time.Now())
